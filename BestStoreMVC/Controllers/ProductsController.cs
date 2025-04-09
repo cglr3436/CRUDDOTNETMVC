98a7d3c55d57a8235e104dc639dd13c158e43ec2
çağlar koçkara <cglr3436@users.noreply.github.com>
2025-04-09 17:04:46 +0300

    Initial commit

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,272 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment environment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            this.environment = environment;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /*
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Brand,Category,Price,Description,ImageFileName,CreatedAt")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( ProductDto productDto)
+        {
+            if (productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "İmage file gerekli");
+                
+            }
+            
+
+                if (ModelState.IsValid)
+                {
+                // save image file
+                string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+                
+                string imageFullPath=environment.WebRootPath +"/products/"+ newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath)) 
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                
+                }
+
+                Product product = new Product()
+                {
+                    Name = productDto.Name,
+                    Brand = productDto.Brand,
+                    Category = productDto.Category, 
+                    Price = productDto.Price,   
+                    Description = productDto.Description,   
+                    ImageFileName = newFileName,
+                    CreatedAt = DateTime.Now,
+                }; 
+                
+                
+                
+                _context.Add(product);
+                    await _context.SaveChangesAsync();
+
+
+
+                    return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(productDto);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ProductDto productDto = new ProductDto() { 
+                Name= product.Name,
+                Brand= product.Brand,
+                Category= product.Category,
+                Price= product.Price,
+                Description= product.Description,
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+
+
+            return View(productDto);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,  ProductDto productDto)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+           
+
+            if (ModelState.IsValid)
+            {
+                // update image file
+                string newFileName=product.ImageFileName;
+                if(productDto.ImageFile != null) {
+                     newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                    newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+                    string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+                    using (var stream = System.IO.File.Create(imageFullPath))
+                    {
+                        productDto.ImageFile.CopyTo(stream);
+
+                    }
+
+                    //delete old
+
+                    string oldimageFullPath = environment.WebRootPath + "/products/" + product.ImageFileName;
+                    System.IO.File.Delete(oldimageFullPath);
+
+                }
+                product.Name = productDto.Name;
+                product.Brand = productDto.Brand;
+                product.Category = productDto.Category;
+                product.Price = productDto.Price;
+                product.Description = productDto.Description;   
+                product.ImageFileName = newFileName;
+              
+
+
+
+
+
+                try
+                {
+
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+            return View(productDto);
+
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                string oldimageFullPath = environment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Delete(oldimageFullPath);
+
+                _context.Products.Remove(product);
+
+               
+
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
